867bca7c6d96a61d08320fdc70cc1454a2ecdc19
Yu Feng <fengyu8299@gmail.com>
2015-04-03 15:20:46 -0500

    initial skeleton

---
+++ src/cs/utexas/wizard/translator/Main.java
@@ -0,0 +1,33 @@
+package cs.utexas.wizard.translator;
+
+import java.io.*;
+import org.antlr.runtime.*;
+import org.antlr.stringtemplate.*;
+import org.antlr.stringtemplate.language.*;
+
+public class Main {
+    public static StringTemplateGroup templates;
+
+    public static void main(String[] args) throws Exception {
+	String templateFileName;
+	int a = 0;
+	if ( args.length<=1 ) {
+		templateFileName = "templates/Java.stg";
+	}
+	else {
+		templateFileName = args[a];
+		a++;
+	}
+	templates = new StringTemplateGroup(new FileReader(templateFileName),
+					    AngleBracketTemplateLexer.class);
+
+	String src = "input";
+	CharStream input = new ANTLRFileStream(src);
+	CMinusLexer lexer = new CMinusLexer(input);
+	CommonTokenStream tokens = new CommonTokenStream(lexer);
+	CMinusParser parser = new CMinusParser(tokens);
+	parser.setTemplateLib(templates);
+	RuleReturnScope r = parser.program();
+	System.out.println(r.getTemplate().toString());
+    }
+}
